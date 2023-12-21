d2ddb4ed60b2b98a221f2e53443cdace5919f759
katebrr <katya.brylinska31@gmail.com>
2023-12-21 18:06:14 +0100

    Push a new view

--- UIKit Today App/ListViewController/ReminderListViewController.swift
+++ UIKit Today App/ListViewController/ReminderListViewController.swift
@@ -26,6 +26,20 @@ class ReminderListViewController: UICollectionViewController {
         updateSnapshot()
         collectionView.dataSource = dataSource
     }
+    
+    override func collectionView(
+        _ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath
+    ) -> Bool {
+        let id = reminders[indexPath.item].id
+        pushDetailView(withId: id)
+        return false
+    }
+    
+    func pushDetailView(withId id: Reminder.ID) {
+        let reminder = reminder(withId: id)
+        let viewController = ReminderViewController(reminder: reminder)
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
