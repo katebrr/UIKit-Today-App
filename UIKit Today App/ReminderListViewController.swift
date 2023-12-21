a0e59bf542530c063bf573d22fdd1504b83740b4
katebrr <katya.brylinska31@gmail.com>
2023-12-21 11:48:32 +0100

    creating a list view

---
+++ UIKit Today App/ReminderListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  UIKit Today App
+//
+//  Created by Katya Brylinska on 21.12.2023.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    var dataSource: DataSource!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+        
+        let cellRegistration = UICollectionView.CellRegistration {
+            (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            var contentConfig = cell.defaultContentConfiguration()
+            contentConfig.text = reminder.title
+            cell.contentConfiguration = contentConfig
+        }
+        
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+        
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(Reminder.sampleData.map { $0.title })
+        
+        dataSource.apply(snapshot)
+        collectionView.dataSource = dataSource
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
+
