6f988746f527bdd2eb845d3e59a2151996244b2f
katebrr <katya.brylinska31@gmail.com>
2023-12-21 10:16:46 +0100

    Empty project

---
+++ UIKit Today App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Today App
+//
+//  Created by Katya Brylinska on 21.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
